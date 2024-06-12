81ed87cc470ce458f69ac259ff9dd664127ead7c
Sweaty Hog <Royceatkins93@gmail.com>
2024-06-12 19:14:01 -0400

    made links on navbar function - about/media

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import Nav from "./Nav";
+import { Link } from 'react-router-dom'
 import ShowLogin from "./ShowLogin";
 
 
@@ -8,7 +8,27 @@ function Header() {
     
     <header className="h-1/2 bg-slate-600 flex">
 
-        <Nav/>
+<div className="flex flex-col justify-evenly h-full w-1/5">
+
+<div className="h-1/3 flex justify-around items-center">
+    {/* <div className="bg-white h-4/5 w-1/2 border rounded-full"></div> */}
+    <img className="h-full" src="/images/logo.png"></img>
+</div>
+
+
+<ul className="flex flex-col items-center">
+   
+    <li className="mb-5"><Link to="/">HOME</Link></li>
+
+    <li className="mb-5"><Link to="/shop">SHOP</Link></li>
+
+    <li className="mb-5"><Link to="/about">ABOUT</Link></li>
+
+    <li className="mb-5"><Link to="/media">MEDIA</Link></li>
+
+</ul>
+
+</div>
 
         <div className="flex flex-col">
             <ShowLogin/>
